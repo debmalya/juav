ac1839db6ac84fad136f110921fae85e23c0eae3
Sri Uma Jayaneesh Adike <asriumajayaneesh@gmail.com>
2015-03-22 18:52:51 -0400

    Stabilization.c done

--- sw/airborne/firmwares/rotorcraft/Stabilization.java
+++ sw/airborne/firmwares/rotorcraft/Stabilization.java
@@ -2,4 +2,13 @@ package sw.airborne.firmwares.rotorcraft;
 
 public class Stabilization {
 
+	public static int stabilization_cmd[] = new int[COMMANDS_NB];
+
+	public static void stabilization_init() {
+		if(! STABILIZATION_SKIP_RATE){
+			stabilization_none_init();
+			stabilization_rate_init();
+		}
+		stabilization_attitude_init();
+	}
 }
