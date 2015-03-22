9186a444c21dbed0d7d265cd24d5fdd9d8dd16a8
Sri Uma Jayaneesh Adike <asriumajayaneesh@gmail.com>
2015-03-22 17:54:11 -0400

    stabilization_none.c done

--- sw/airborne/firmwares/rotorcraft/stabilization/Stabilization_none.java
+++ sw/airborne/firmwares/rotorcraft/stabilization/Stabilization_none.java
@@ -1,5 +1,27 @@
 package sw.airborne.firmwares.rotorcraft.stabilization;
 
 public class Stabilization_none {
+	public static Int32Rates stabilization_none_rc_cmd;
 
+	public static void stabilization_none_init() {
+		INT_RATES_ZERO(stabilization_none_rc_cmd);
+	}
+
+	public static void stabilization_none_read_rc(  ) {
+
+		stabilization_none_rc_cmd.p = (int)radio_control.values[RADIO_ROLL];
+		stabilization_none_rc_cmd.q = (int)radio_control.values[RADIO_PITCH];
+		stabilization_none_rc_cmd.r = (int)radio_control.values[RADIO_YAW];
+	}
+
+	public static void stabilization_none_enter() {
+		INT_RATES_ZERO(stabilization_none_rc_cmd);
+	}
+
+	public static void stabilization_none_run(boolean in_flight) {
+		/* just directly pass rc commands through */
+		stabilization_cmd[COMMAND_ROLL]  = stabilization_none_rc_cmd.p;
+		stabilization_cmd[COMMAND_PITCH] = stabilization_none_rc_cmd.q;
+		stabilization_cmd[COMMAND_YAW]   = stabilization_none_rc_cmd.r;
+	}
 }
