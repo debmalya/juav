84dfba819583d32b63520986ca06c8b8d38b4952
Weihao Qu <weihaoqu@Weihaos-MacBook-Pro.local>
2015-03-25 23:43:21 -0400

    add nps_main.c

--- sw/simulator/nps/Nps_main.java
+++ sw/simulator/nps/Nps_main.java
@@ -1,5 +1,10 @@
 package sw.simulator.nps;
-
-public class Nps_main {
+interface Nps_main{
+    bool_t nps_main_parse_options(int argc, char** argv);
+    void nps_main_init(void);
+    void nps_main_display(void);
+    void nps_main_run_sim_step(void);
+    boolean nps_main_periodic(gpointer data __attribute__ ((unused)));}
+public class Nps_main implements Nps_main {
 
 }
