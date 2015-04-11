e6e2e05a784b5e00c9a8b3493501977c964a6ea9
vasanth <vsubrama@buffalo.edu>
2015-04-11 00:02:59 -0400

    Further GPS changes GPS receive completed Gyro and AHRS left

--- devices/Gps.java
+++ devices/Gps.java
@@ -61,78 +61,74 @@ public class Gps {
 	
 	//void parse_gps_datalink(uint8_t numsv, int32_t ecef_x, int32_t ecef_y, int32_t ecef_z, int32_t lat, int32_t lon, int32_t alt,
 	//int32_t hmsl, int32_t ecef_xd, int32_t ecef_yd, int32_t ecef_zd, uint32_t tow, int32_t course);
-	public void parse_data_link(int numsv, int ecef_x, int ecef_y, int ecef_z, int lat, 
-			int lon, int alt,int hmsl, int ecef_xd, int ecef_yd, int ecef_zd, int tow, int course){
-		 gps.lla_pos.lat = RadOfDeg(lat);
-		  gps.lla_pos.lon = RadOfDeg(lon);
-		  gps.lla_pos.alt = alt;
-		  gps.hmsl        = hmsl;
-
-		  gps.ecef_pos.x = ecef_x;
-		  gps.ecef_pos.y = ecef_y;
-		  gps.ecef_pos.z = ecef_z;
-
-		  gps.ecef_vel.x = ecef_xd;
-		  gps.ecef_vel.y = ecef_yd;
-		  gps.ecef_vel.z = ecef_zd;
-
-		  gps.course = course;
-		  gps.num_sv = numsv;
-		  gps.tow = tow;
-		  gps.fix = GPS_FIX_3D;
-		  gps_available = true;
-		  
-	}
-	void  gps_feed_value() {
-		  gps.ecef_pos.x = sensors.gps.ecef_pos.x * 100.;
-		  gps.ecef_pos.y = sensors.gps.ecef_pos.y * 100.;
-		  gps.ecef_pos.z = sensors.gps.ecef_pos.z * 100.;
-		  gps.ecef_vel.x = sensors.gps.ecef_vel.x * 100.;
-		  gps.ecef_vel.y = sensors.gps.ecef_vel.y * 100.;
-		  gps.ecef_vel.z = sensors.gps.ecef_vel.z * 100.;
+//	public void parse_data_link(int numsv, int ecef_x, int ecef_y, int ecef_z, int lat, 
+//			int lon, int alt,int hmsl, int ecef_xd, int ecef_yd, int ecef_zd, int tow, int course){
+//		 gps.lla_pos.lat = RadOfDeg(lat);
+//		  gps.lla_pos.lon = RadOfDeg(lon);
+//		  gps.lla_pos.alt = alt;
+//		  gps.hmsl        = hmsl;
+//
+//		  gps.ecef_pos.x = ecef_x;
+//		  gps.ecef_pos.y = ecef_y;
+//		  gps.ecef_pos.z = ecef_z;
+//
+//		  gps.ecef_vel.x = ecef_xd;
+//		  gps.ecef_vel.y = ecef_yd;
+//		  gps.ecef_vel.z = ecef_zd;
+//
+//		  gps.course = course;
+//		  gps.num_sv = numsv;
+//		  gps.tow = tow;
+//		  gps.fix = GPS_FIX_3D;
+//		  gps_available = true;
+//		  
+//	}
+	void  gps_feed_value(Long[] values) {
+		  gps.ecef_pos.x = values[1];
+		  gps.ecef_pos.y = values[2];
+		  gps.ecef_pos.z = values[3];
+		  gps.ecef_vel.x = values[4];
+		  gps.ecef_vel.y = values[5];
+		  gps.ecef_vel.z = values[6];
 		  //ecef pos seems to be based on geocentric model, hence we get a very high alt when converted to lla
-		  gps.lla_pos.lat = sensors.gps.lla_pos.lat * 1e7;
-		  gps.lla_pos.lon = sensors.gps.lla_pos.lon * 1e7;
-		  gps.lla_pos.alt = sensors.gps.lla_pos.alt * 1000.;
-		  gps.hmsl        = sensors.gps.hmsl * 1000.;
+		  gps.lla_pos.lat = values[7];
+		  gps.lla_pos.lon = values[8];
+		  gps.lla_pos.alt = values[9];
+		  gps.hmsl        = values[10];
 
 		  /* calc NED speed from ECEF */
-		  struct LtpDef_d ref_ltp;
-		  ltp_def_from_ecef_d(&ref_ltp, &sensors.gps.ecef_pos);
-		  struct NedCoor_d ned_vel_d;
-		  ned_of_ecef_vect_d(&ned_vel_d, &ref_ltp, &sensors.gps.ecef_vel);
-		  gps.ned_vel.x = ned_vel_d.x * 100;
-		  gps.ned_vel.y = ned_vel_d.y * 100;
-		  gps.ned_vel.z = ned_vel_d.z * 100;
+		  gps.ned_vel.x = values[11];
+		  gps.ned_vel.y = values[12];
+		  gps.ned_vel.z = values[13];
 
 		  /* horizontal and 3d ground speed in cm/s */
-		  gps.gspeed = sqrt(ned_vel_d.x * ned_vel_d.x + ned_vel_d.y * ned_vel_d.y) * 100;
-		  gps.speed_3d = sqrt(ned_vel_d.x * ned_vel_d.x + ned_vel_d.y * ned_vel_d.y + ned_vel_d.z * ned_vel_d.z) * 100;
+		  gps.gspeed = values[14];
+		  gps.speed_3d = values[15];
 
 		  /* ground course in radians * 1e7 */
-		  gps.course = atan2(ned_vel_d.y, ned_vel_d.x) * 1e7;
+		  gps.course = values[16];
 
-		#if GPS_USE_LATLONG
-		  /* Computes from (lat, long) in the referenced UTM zone */
-		  struct LlaCoor_f lla_f;
-		  lla_f.lat = ((float) gps.lla_pos.lat) / 1e7;
-		  lla_f.lon = ((float) gps.lla_pos.lon) / 1e7;
-		  struct UtmCoor_f utm_f;
-		  utm_f.zone = nav_utm_zone0;
-		  /* convert to utm */
-		  utm_of_lla_f(&utm_f, &lla_f);
-		  /* copy results of utm conversion */
-		  gps.utm_pos.east = utm_f.east*100;
-		  gps.utm_pos.north = utm_f.north*100;
-		  gps.utm_pos.alt = gps.lla_pos.alt;
-		  gps.utm_pos.zone = nav_utm_zone0;
-		#endif
+//		#if GPS_USE_LATLONG
+//		  /* Computes from (lat, long) in the referenced UTM zone */
+//		  struct LlaCoor_f lla_f;
+//		  lla_f.lat = ((float) gps.lla_pos.lat) / 1e7;
+//		  lla_f.lon = ((float) gps.lla_pos.lon) / 1e7;
+//		  struct UtmCoor_f utm_f;
+//		  utm_f.zone = nav_utm_zone0;
+//		  /* convert to utm */
+//		  utm_of_lla_f(&utm_f, &lla_f);
+//		  /* copy results of utm conversion */
+//		  gps.utm_pos.east = utm_f.east*100;
+//		  gps.utm_pos.north = utm_f.north*100;
+//		  gps.utm_pos.alt = gps.lla_pos.alt;
+//		  gps.utm_pos.zone = nav_utm_zone0;
+//		#endif
 
 		  if (gps_has_fix)
 		    gps.fix = GPS_FIX_3D;
 		  else
 		    gps.fix = GPS_FIX_NONE;
-		  gps_available = TRUE;
+		  gps_available = true;
 		}
 	
 	public void gps_impl_init(){
--- sw/communication/Commchannel.java
+++ sw/communication/Commchannel.java
@@ -1,4 +1,6 @@
 package sw.communication;
+import java.util.StringTokenizer;
+
 import fr.dgac.ivy.* ;
 import devices.GpsState;
 
@@ -9,10 +11,18 @@ public class Commchannel implements IvyMessageListener {
 
 	public void CommChannel() throws IvyException {
 		 bus = new Ivy("JUAV","JUAV Ready",null);
-		 bus.bindMsg("NPS_SEN_NICE_GPS(.*)",new IvyMessageListener() {
+		 bus.bindMsg("GPS_CALCULAED_DEVICE(.*)",new IvyMessageListener() {
 		      public void receive(IvyClient client, String[] args) {
 			// Process GPS
-		        
+		    	Long[] gpsState = new Long[17];
+		    	int i=0;
+		    	 StringTokenizer tokenizedString = new StringTokenizer(args[0], " ");
+		    	 while (tokenizedString.hasMoreElements()) {
+					String currentString = tokenizedString.nextToken();
+					gpsState[i] = Long.parseLong(currentString);
+					i++;
+				}
+		    	 
 		      }
 		    });
 		 bus.bindMsg("^AHRS(.*)",new IvyMessageListener() {
